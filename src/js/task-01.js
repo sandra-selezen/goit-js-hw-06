3e784de22774cfee9e97289b0c84f08a6eade605
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-01-27 20:01:40 +0200

    Update task-01.js
    Add task #1

---
+++ src/js/task-01.js
@@ -0,0 +1,29 @@
+const itemsEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${itemsEl.length}`);
+
+itemsEl.forEach((item) => {
+    console.log('Category: ', item.firstElementChild.textContent);
+    console.log('Elements: ', item.lastElementChild.children.length);
+});
+
+/* 
+HTML містить список категорій ul#categories.
+Напиши скрипт, який:
+
+1. Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+2. Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль 
+текст заголовку елемента (тегу <h2>)
+і кількість елементів в категорії (усіх <li>, вкладених в нього).
+В результаті, в консолі будуть виведені наступні повідомлення.
+
+Number of categories: 3
+
+Category: Animals
+Elements: 4
+
+Category: Products
+Elements: 3
+
+Category: Technologies
+Elements: 5
+*/
