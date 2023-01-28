159102e5344679724acc64e88944d3e602e80d1b
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-01-28 22:32:25 +0200

    Update task-04.js
    Add task #4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,6 +1,21 @@
 let counterValue = 0;
 
+const valueEl = document.querySelector('#value');
+const incrementBtnEl = document.querySelector('button[data-action="increment"]');
+const decrementBtnEl = document.querySelector('button[data-action="decrement"]');
 
+const onIncrementBtnClick = () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+}
+
+const onDecrementBtnClick = () => {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
+
+incrementBtnEl.addEventListener("click", onIncrementBtnClick);
+decrementBtnEl.addEventListener("click", onDecrementBtnClick);
 
 /*
 Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати
